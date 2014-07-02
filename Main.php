e448f0116970fa3500e456b3496acdc49a87b969
Ben Werdmuller <ben@benwerd.com>
2014-07-02 11:04:16 -0700

    Autoload AWS SDK in an intelligent way

--- Main.php
+++ Main.php
@@ -6,30 +6,33 @@
 
             function registerEventHooks() {
 
-                // Load the AWS SDK
-                //require_once dirname(__FILE__) . '/external/aws-sdk/build/aws-autoloader.php';
+                // All of this only makes sense if we have an aws_key and aws_secret
 
-                // Load AWS SDK and dependencies
-                $classLoader = new \Symfony\Component\ClassLoader\UniversalClassLoader();
-                $classLoader->registerNamespaces(array(
-                    'Aws'      => dirname(__FILE__) . '/external/aws-sdk/src',
-                    'Guzzle'   => dirname(__FILE__) . '/external/guzzle/src',
-                    'Doctrine' => dirname(__FILE__) . '/external/doctrine/lib/Doctrine',
-                    'Psr'      => dirname(__FILE__) . '/external/psrlog',
-                    'Monolog'  => dirname(__FILE__) . '/external/monolog/src'
-                ));
+                if (!empty(\Idno\Core\site()->config()->aws_key) && !empty(\Idno\Core\site()->config()->aws_secret)) {
 
-                $classLoader->register();
+                    // Load AWS SDK and dependencies
+                    $classLoader = new \Symfony\Component\ClassLoader\UniversalClassLoader();
+                    $classLoader->registerNamespaces(array(
+                        'Aws'      => dirname(__FILE__) . '/external/aws-sdk/src',
+                        'Guzzle'   => dirname(__FILE__) . '/external/guzzle/src',
+                        'Doctrine' => dirname(__FILE__) . '/external/doctrine/lib/Doctrine',
+                        'Psr'      => dirname(__FILE__) . '/external/psrlog',
+                        'Monolog'  => dirname(__FILE__) . '/external/monolog/src'
+                    ));
 
-                $aws = \Aws\Common\Aws::factory(array(
-                    'key'     => '',
-                    'secret'  => '',
-                    'region'  => 'us-east-1',
-                ));
+                    $classLoader->register();
+
+                    $aws = \Aws\Common\Aws::factory(array(
+                        'key'     => \Idno\Core\site()->config()->aws_key,
+                        'secret'  => \Idno\Core\site()->config()->aws_secret,
+                        'region'  => 'us-east-1',
+                    ));
 
 
-                // Impose the S3 filesystem
-                \Idno\Core\site()->filesystem = new \IdnoPlugins\S3\S3FileSystem();
+                    // Impose the S3 filesystem
+                    \Idno\Core\site()->filesystem = new \IdnoPlugins\S3\S3FileSystem();
+
+                }
 
             }
 
