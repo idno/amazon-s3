a8210b63264519f698aee2e9512043ab93b8c632
Ben Werdmuller <ben@benwerd.com>
2014-07-01 18:24:23 -0700

    Properly autoloading.

--- Main.php
+++ Main.php
@@ -6,6 +6,28 @@
 
             function registerEventHooks() {
 
+                // Load the AWS SDK
+                //require_once dirname(__FILE__) . '/external/aws-sdk/build/aws-autoloader.php';
+
+                // Load AWS SDK and dependencies
+                $classLoader = new \Symfony\Component\ClassLoader\UniversalClassLoader();
+                $classLoader->registerNamespaces(array(
+                    'Aws'      => dirname(__FILE__) . '/external/aws-sdk/src',
+                    'Guzzle'   => dirname(__FILE__) . '/external/guzzle/src',
+                    'Doctrine' => dirname(__FILE__) . '/external/doctrine/lib/Doctrine',
+                    'Psr'      => dirname(__FILE__) . '/external/psrlog',
+                    'Monolog'  => dirname(__FILE__) . '/external/monolog/src'
+                ));
+
+                $classLoader->register();
+
+                $aws = \Aws\Common\Aws::factory(array(
+                    'key'     => '',
+                    'secret'  => '',
+                    'region'  => 'us-east-1',
+                ));
+
+
                 // Impose the S3 filesystem
                 \Idno\Core\site()->filesystem = new \IdnoPlugins\S3\S3FileSystem();
 
